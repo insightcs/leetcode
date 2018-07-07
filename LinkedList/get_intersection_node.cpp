ee5d7bb18b25c39b757945f53de219e0b92fc993
insightcs <ljw0608.mail@gmail.com>
2018-07-07 16:38:50 +0800

    update

--- LinkedList/get_intersection_node.cpp
+++ LinkedList/get_intersection_node.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <set>
+
 typedef struct ListNode
 {
     int val;
@@ -9,11 +12,77 @@ typedef struct ListNode
     }
 }ListNode;
 
+/*iterative:
+ * time: O(n1+n2)
+ * memory: O(n1)*/
 class Solution
 {
 public:
     ListNode* getIntersectionNode(ListNode* headA, ListNode* headB)
     {
+        std::set<ListNode*> s;
+        while(headA)
+        {
+            s.insert(headA);
+            headA = headA->next;
+        }
+        if(s.empty())
+        {
+            return nullptr;
+        }
+        while(headB)
+        {
+            if(s.find(headB)!=s.end())
+            {
+                return headB;
+            }
+            headB = headB->next;
+        }
+        return nullptr;
+    }
+};
 
+/*iterative
+ * time: O(n1*n2)
+ * memory: O(1)*/
+class Solution
+{
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB)
+    {
+        ListNode *p = nullptr;
+        while(headA)
+        {
+            p = headB;
+            while(p)
+            {
+                if(headA == p)
+                {
+                    return headA;
+                }
+                p = p->next;
+            }
+            headA = headA->next;
+        }
+        return nullptr;
+    }
+};
+
+/*iterative
+ * time: O(n1+n2)
+ * memory: O(1)*/
+class Solution
+{
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB)
+    {
+        ListNode *p_headA = headA;
+        ListNode *p_headB = headB;
+        while(p_headA != p_headB)
+        {
+            p_headA ? p_headA->next : headB;
+            p_headB ? p_headB->next : headA;
+        }
+        return p_headA;
     }
 };
