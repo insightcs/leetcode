15e23899c816b7de60a630d9b36d05ff577ee461
insightcs <ljw0608.mail@gmail.com>
2018-07-09 21:37:07 +0800

    update

--- tree/maximum_depth_binary_tree.cpp
+++ tree/maximum_depth_binary_tree.cpp
@@ -43,3 +43,13 @@ private:
         }
     }
 };
+
+class Solution
+{
+public:
+    int maxDepth(TreeNode* root)
+    {
+        if(!root)   return 0;
+        return max(maxDepth(root->left), maxDepth(root->right))+1;
+    }
+};
