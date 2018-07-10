57ec30647a84808121e8bf139f8e44cb2a1044f4
insightcs <ljw0608.mail@gmail.com>
2018-07-10 12:17:44 +0800

    update

---
+++ tree/find_target.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <set>
+#include <queue>
+#include <vector>
+using namespace std;
+
+typedef struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+}TreeNode;
+
+
+/* DFS + set
+ * time: O(N)
+ * memory: O(N) */
+class Solution
+{
+public:
+    bool findTarget(TreeNode* root, int k)
+    {
+        set<int> s;
+        return helper(root, k, s);
+    }
+
+private:
+    bool helper(TreeNode* root, int k, set<int> &s)
+    {
+        if(!root)
+        {
+            return false;
+        }
+        if(s.find(k-root->val)!=s.end())
+        {
+            return true;
+        }
+        s.insert(root->val);
+        return helper(root->left, k, s) || helper(root->right, k, s);
+    }
+};
+
+/* BFS + set
+ * time: O(N)
+ * memory: O(N) */
+class Solution
+{
+public:
+    bool findTarget(TreeNode* root, int k)
+    {
+        if(!root)   return false;
+        set<int> s;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            TreeNode* node = q.front();
+            q.pop();
+            if(s.find(k-node->val)!=s.end())
+            {
+                return true;
+            }
+            s.insert(node->val);
+            if(node->left)
+            {
+                q.push(node->left);
+            }
+            if(node->right)
+            {
+                q.push(node->right);
+            }
+        }
+        return false;
+    }
+};
+
+/* BST
+ * time: O(NH)
+ * memory: O(H) */
+class Solution
+{
+public:
+    bool findTarget(TreeNode* root, int k)
+    {
+        return dfs(root, root, k);
+    }
+private:
+    bool dfs(TreeNode* root, TreeNode* node, int k)
+    {
+        if(!node)   return false;
+        return search(root, node, k-node->val) || dfs(root, node->left, k) || dfs(root, node->right, k);
+    }
+    bool search(TreeNode* root, TreeNode* node, int value)
+    {
+        if (!root)
+        {
+            return false;
+        }
+        if(root!=node && root->val==value)
+        {
+            return true;
+        }
+        if(root->val>value)
+        {
+            return search(root->left, node, value);
+        }
+        if(root->val<value)
+        {
+            return search(root->right, node, value);
+        }
+        return false;
+    }
+};
+
+/* BST
+ * time: O(N)
+ * memory: O(n) */
+class Solution
+{
+public:
+    bool findTarget(TreeNode* root, int k)
+    {
+        vector<int> list;
+        inorder_traversal(root, list);
+        unsigned int start = 0;
+        unsigned int end = list.size()-1;
+        while(start<end)
+        {
+            if(list[start]+list[end]==k)
+            {
+                return true;
+            }
+            if(list[start]+list[end]<k)
+            {
+                start++;
+            }
+            else
+            {
+                end--;
+            }
+        }
+        return false;
+    }
+
+private:
+    void inorder_traversal(TreeNode* root, vector<int> &list)
+    {
+        if(!root)   return;
+        inorder_traversal(root->left, list);
+        list.push_back(root->val);
+        inorder_traversal(root->right, list);
+    }
+};
---
+++ tree/invert_tree.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+typedef struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x)
+    {
+        this->val = x;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+}TreeNode;
+
+/* recursive
+ * time: O(N)
+ * memory: O(H)*/
+class Solution
+{
+public:
+    TreeNode* invertTree(TreeNode* root)
+    {
+        if(!root)   return root;
+        TreeNode* temp = root->right;
+        root->right = invertTree(root->left);
+        root->left = invertTree(temp);
+        return root;
+    }
+};
+
+/* iterative
+ * time: O(N)
+ * memory: O(N)*/
+class Solution
+{
+public:
+    TreeNode* invertTree(TreeNode* root)
+    {
+        if(!root)   return root;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            TreeNode* node = q.front();
+            q.pop();
+            if(node->left)
+            {
+                q.push(node->left);
+            }
+            if(node->right)
+            {
+                q.push(node->right);
+            }
+            TreeNode* temp = node->left;
+            node->left = node->right;
+            node->right = temp;
+        }
+        return root;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    TreeNode *root = new TreeNode(4);
+    TreeNode *p = root;
+    p->left = new TreeNode(2);
+    p->right = new TreeNode(7);
+    p->left->left = new TreeNode(1);
+    p->left->right = new TreeNode(3);
+    p->right->left = new TreeNode(6);
+    p->right->right = new TreeNode(9);
+
+    Solution *s = new Solution;
+    root = s->invertTree(root);
+    delete s;
+    return 0;
+}
