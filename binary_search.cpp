2d33a48f3a0db1221af2a5381343c773b0cd2fa3
insightcs <ljw0608.mail@gmail.com>
2018-08-18 12:52:42 +0800

    update

---
+++ binary_search.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+bool binary_search(vector<int>& nums, int start, int end)
+{
+    int mid = (start + end) / 2;
+    if(nums[mid]==)
+    {
+        return true;
+    }
+    else if(nums[mid]>val)
+    {
+        end--;
+    }
+}
